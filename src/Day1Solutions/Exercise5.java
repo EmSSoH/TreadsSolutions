8b3ae1c28420cdbbc83855303dfded08eedeadf1
Magnus West Madsen <cph-mm454@cphbusiness.dk>
2018-12-04 23:41:21 +0100

    exercise 5

---
+++ src/Day1Solutions/Exercise5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day1Solutions;
+
+/**
+ *
+ * @author Magnus West Madsen
+ */
+public class Exercise5 {
+    
+    /*
+    
+    i am having problems with the java program not allowing me to press stop or X out the program even after extending thread.
+    
+    though i do understand that being able to start up multiple threads to run each ball seperetly would free up the main to run the program.
+    
+    i am not sure if it is kernels or cores that have multiple threads. but a single thread would not be able to fix it in this way.
+    
+    */
+    
+}
