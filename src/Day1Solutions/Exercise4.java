add1109a47e6820486d89c45a447a2756d7d0677
Magnus West Madsen <cph-mm454@cphbusiness.dk>
2018-12-04 23:22:42 +0100

    exercise 4

---
+++ src/Day1Solutions/Exercise4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day1Solutions;
+
+/**
+ *
+ * @author Magnus West Madsen
+ */
+public class Exercise4 {
+    /*
+        Synchronized makes it so if a method that is Synchronized is being run by a thread,
+        the method or object that the method is on becomes locked from other threads to use until the thread using it is done with it.
+    
+        volatile makes it so a veriable that is volatile never gets cached thread-locally and all interactions are made with it through the main memory
+        therefor not blocking other threads from using the veriable but still acting as if the veriable is synchronized itself.
+    
+    */
+}
